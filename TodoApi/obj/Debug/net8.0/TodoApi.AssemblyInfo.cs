6ae1a83a5b303dcbe47dfd0a627fa4bf23ade265
Yusa21 <157543456+Yusa21@users.noreply.github.com>
2025-03-10 11:58:33 +0100

    que mas da

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cbd9e82ad1233810136acf75b3d2027ce83e160")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec40fa7cc089b2cb34454ec8ce2addb52bc1ddc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
